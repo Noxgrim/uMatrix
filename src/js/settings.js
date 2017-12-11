Noxgrim/uMatrix

commit 68dedc8111d3b1e27a3d9253cd79f92d0fe8ae56

Fix #335
Additionally, performance improvements:
- Reduce overhead of collapsing elements
  (see https://github.com/gorhill/uBlock/issues/2839)
- Cache decomposition of source hostname when matrix-filtering

Also, various code review.

src/js/settings.js
@@ -53,16 +53,16 @@ function changeMatrixSwitch(name, state) {
 
 /******************************************************************************/
 
-function onChangeValueHandler(uelem, setting, min, max) {
+function onChangeValueHandler(elem, setting, min, max) {
     var oldVal = cachedSettings.userSettings[setting];
-    var newVal = Math.round(parseFloat(uelem.val()));
+    var newVal = Math.round(parseFloat(elem.value));
     if ( typeof newVal !== 'number' ) {
         newVal = oldVal;
     } else {
         newVal = Math.max(newVal, min);
         newVal = Math.min(newVal, max);
     }
-    uelem.val(newVal);
+    elem.value = newVal;
     if ( newVal !== oldVal ) {
         changeUserSettings(setting, newVal);
     }
@@ -71,50 +71,89 @@ function onChangeValueHandler(uelem, setting, min, max) {
 /******************************************************************************/
 
 function prepareToDie() {
-    onChangeValueHandler(uDom('#delete-unused-session-cookies-after'), 'deleteUnusedSessionCookiesAfter', 15, 1440);
-    onChangeValueHandler(uDom('#clear-browser-cache-after'), 'clearBrowserCacheAfter', 15, 1440);
+    onChangeValueHandler(
+        uDom.nodeFromId('deleteUnusedSessionCookiesAfter'),
+        'deleteUnusedSessionCookiesAfter',
+        15, 1440
+    );
+    onChangeValueHandler(
+        uDom.nodeFromId('clearBrowserCacheAfter'),
+        'clearBrowserCacheAfter',
+        15, 1440
+    );
 }
 
 /******************************************************************************/
 
-var installEventHandlers = function() {
-    uDom('input[name="displayTextSize"]').on('change', function(){
-        changeUserSettings('displayTextSize', this.value);
-    });
+function onInputChanged(ev) {
+    var target = ev.target;
 
-    uDom('#popupScopeLevel').on('change', function(){
-        changeUserSettings('popupScopeLevel', this.value);
-    });
+    switch ( target.id ) {
+    case 'displayTextSizeNormal':
+    case 'displayTextSizeLarge':
+        changeUserSettings('displayTextSize', target.value);
+        break;
+    case 'clearBrowserCache':
+    case 'cloudStorageEnabled':
+    case 'collapseBlacklisted':
+    case 'collapseBlocked':
+    case 'colorBlindFriendly':
+    case 'deleteCookies':
+    case 'deleteLocalStorage':
+    case 'deleteUnusedSessionCookies':
+    case 'iconBadgeEnabled':
+    case 'processHyperlinkAuditing':
+        changeUserSettings(target.id, target.checked);
+        break;
+    case 'noMixedContent':
+    case 'processReferer':
+        changeMatrixSwitch(
+            target.getAttribute('data-matrix-switch'),
+            target.checked
+        );
+        break;
+    case 'deleteUnusedSessionCookiesAfter':
+        onChangeValueHandler(target, 'deleteUnusedSessionCookiesAfter', 15, 1440);
+        break;
+    case 'clearBrowserCacheAfter':
+        onChangeValueHandler(target, 'clearBrowserCacheAfter', 15, 1440);
+        break;
+    case 'popupScopeLevel':
+        changeUserSettings('popupScopeLevel', target.value);
+        break;
+    default:
+        break;
+    }
 
-    uDom('[data-setting-bool]').on('change', function(){
-        var settingName = this.getAttribute('data-setting-bool');
-        if ( typeof settingName === 'string' && settingName !== '' ) {
-            changeUserSettings(settingName, this.checked);
-        }
-    });
-
-    uDom('[data-matrix-switch]').on('change', function(){
-        var switchName = this.getAttribute('data-matrix-switch');
-        if ( typeof switchName === 'string' && switchName !== '' ) {
-            changeMatrixSwitch(switchName, this.checked);
-        }
-    });
-
-    uDom('#delete-unused-session-cookies-after').on('change', function(){
-        onChangeValueHandler(uDom(this), 'deleteUnusedSessionCookiesAfter', 15, 1440);
-    });
-    uDom('#clear-browser-cache-after').on('change', function(){
-        onChangeValueHandler(uDom(this), 'clearBrowserCacheAfter', 15, 1440);
-    });
-
-    // https://github.com/gorhill/httpswitchboard/issues/197
-    uDom(window).on('beforeunload', prepareToDie);
-};
+    switch ( target.id ) {
+    case 'collapseBlocked':
+        synchronizeWidgets();
+        break;
+    default:
+        break;
+    }
+}
 
 /******************************************************************************/
 
-uDom.onLoad(function() {
-    var onSettingsReceived = function(settings) {
+function synchronizeWidgets() {
+    var e1, e2;
+
+    e1 = uDom.nodeFromId('collapseBlocked');
+    e2 = uDom.nodeFromId('collapseBlacklisted');
+    if ( e1.checked ) {
+        e2.setAttribute('disabled', '');
+    } else {
+        e2.removeAttribute('disabled');
+    }
+}
+
+/******************************************************************************/
+
+vAPI.messaging.send(
+    'settings.js',
+    { what: 'getUserSettings' },
+    function onSettingsReceived(settings) {
         // Cache copy
         cachedSettings = settings;
 
@@ -122,10 +161,7 @@ uDom.onLoad(function() {
         var matrixSwitches = settings.matrixSwitches;
 
         uDom('[data-setting-bool]').forEach(function(elem){
-            var settingName = elem.attr('data-setting-bool');
-            if ( typeof settingName === 'string' && settingName !== '' ) {
-                elem.prop('checked', userSettings[settingName] === true);
-            }
+            elem.prop('checked', userSettings[elem.prop('id')] === true);
         });
 
         uDom('[data-matrix-switch]').forEach(function(elem){
@@ -140,18 +176,19 @@ uDom.onLoad(function() {
         });
 
         uDom.nodeFromId('popupScopeLevel').value = userSettings.popupScopeLevel;
+        uDom.nodeFromId('deleteUnusedSessionCookiesAfter').value =
+            userSettings.deleteUnusedSessionCookiesAfter;
+        uDom.nodeFromId('clearBrowserCacheAfter').value =
+            userSettings.clearBrowserCacheAfter;
 
-        uDom('#delete-unused-session-cookies-after').val(userSettings.deleteUnusedSessionCookiesAfter);
-        uDom('#clear-browser-cache-after').val(userSettings.clearBrowserCacheAfter);
+        synchronizeWidgets();
 
-        installEventHandlers();
-    };
-    vAPI.messaging.send(
-        'settings.js',
-        { what: 'getUserSettings' },
-        onSettingsReceived
-    );
-});
+        document.addEventListener('change', onInputChanged);
+
+        // https://github.com/gorhill/httpswitchboard/issues/197
+        uDom(window).on('beforeunload', prepareToDie);
+    }
+);
 
 /******************************************************************************/
 

src/css/dashboard-common.css
@@ -36,7 +36,9 @@ a {
 button {
     padding: 0.3em 0.5em;
     }
-
+input[disabled] + label {
+    color: gray;
+    }
 .para {
     width: 40em;
     }

src/js/background.js
@@ -91,6 +91,7 @@ return {
         clearBrowserCache: true,
         clearBrowserCacheAfter: 60,
         cloudStorageEnabled: false,
+        collapseBlacklisted: true,
         collapseBlocked: false,
         colorBlindFriendly: false,
         deleteCookies: false,

src/js/contentscript.js
@@ -109,71 +109,86 @@ vAPI.contentscriptEndInjected = true;
 // https://github.com/gorhill/uMatrix/issues/45
 
 var collapser = (function() {
-    var timer = null;
-    var requestId = 1;
-    var newRequests = [];
-    var pendingRequests = {};
-    var pendingRequestCount = 0;
-    var srcProps = {
-        'img': 'src'
+    var resquestIdGenerator = 1,
+        processTimer,
+        toProcess = [],
+        toFilter = [],
+        toCollapse = new Map(),
+        cachedBlockedMap,
+        cachedBlockedMapHash,
+        cachedBlockedMapTimer,
+        reURLPlaceholder = /\{\{url\}\}/g;
+    var src1stProps = {
+        'embed': 'src',
+        'iframe': 'src',
+        'img': 'src',
+        'object': 'data'
     };
-    var reURLplaceholder = /\{\{url\}\}/g;
-
-    var PendingRequest = function(target) {
-        this.id = requestId++;
-        this.target = target;
-        pendingRequests[this.id] = this;
-        pendingRequestCount += 1;
+    var src2ndProps = {
+        'img': 'srcset'
+    };
+    var tagToTypeMap = {
+        embed: 'media',
+        iframe: 'frame',
+        img: 'image',
+        object: 'media'
+    };
+    var cachedBlockedSetClear = function() {
+        cachedBlockedMap =
+        cachedBlockedMapHash =
+        cachedBlockedMapTimer = undefined;
     };
 
-    // Because a while ago I have observed constructors are faster than
-    // literal object instanciations.
-    var BouncingRequest = function(id, tagName, url) {
-        this.id = id;
-        this.tagName = tagName;
-        this.url = url;
-        this.blocked = false;
-    };
-
+    // https://github.com/chrisaljoudi/uBlock/issues/174
+    //   Do not remove fragment from src URL
     var onProcessed = function(response) {
-        if ( !response ) {
+        if ( !response ) { // This happens if uBO is disabled or restarted.
+            toCollapse.clear();
             return;
         }
-        var requests = response.requests;
-        if ( requests === null || Array.isArray(requests) === false ) {
+
+        var targets = toCollapse.get(response.id);
+        if ( targets === undefined ) { return; }
+        toCollapse.delete(response.id);
+        if ( cachedBlockedMapHash !== response.hash ) {
+            cachedBlockedMap = new Map(response.blockedResources);
+            cachedBlockedMapHash = response.hash;
+            if ( cachedBlockedMapTimer !== undefined ) {
+                clearTimeout(cachedBlockedMapTimer);
+            }
+            cachedBlockedMapTimer = vAPI.setTimeout(cachedBlockedSetClear, 30000);
+        }
+        if ( cachedBlockedMap === undefined || cachedBlockedMap.size === 0 ) {
             return;
         }
-        var collapse = response.collapse;
-        var placeholders = response.placeholders;
-        var i = requests.length;
-        var request, entry, target, tagName, docurl, replaced;
-        while ( i-- ) {
-            request = requests[i];
-            if ( pendingRequests.hasOwnProperty(request.id) === false ) {
-                continue;
+
+        var placeholders = response.placeholders,
+            tag, prop, src, collapsed, docurl, replaced;
+
+        for ( var target of targets ) {
+            tag = target.localName;
+            prop = src1stProps[tag];
+            if ( prop === undefined ) { continue; }
+            src = target[prop];
+            if ( typeof src !== 'string' || src.length === 0 ) {
+                prop = src2ndProps[tag];
+                if ( prop === undefined ) { continue; }
+                src = target[prop];
+                if ( typeof src !== 'string' || src.length === 0 ) { continue; }
             }
-            entry = pendingRequests[request.id];
-            delete pendingRequests[request.id];
-            pendingRequestCount -= 1;
-
-            // Not blocked
-            if ( !request.blocked ) {
-                continue;
-            }
-
-            target = entry.target;
-
-            // No placeholders
-            if ( collapse ) {
+            collapsed = cachedBlockedMap.get(tagToTypeMap[tag] + ' ' + src);
+            if ( collapsed === undefined ) { continue; }
+            if ( collapsed ) {
                 target.style.setProperty('display', 'none', 'important');
+                target.hidden = true;
                 continue;
             }
-
-            tagName = target.localName;
-
-            // Special case: iframe
-            if ( tagName === 'iframe' ) {
-                docurl = 'data:text/html,' + encodeURIComponent(placeholders.iframe.replace(reURLplaceholder, request.url));
+            if ( tag === 'iframe' ) {
+                docurl =
+                    'data:text/html,' +
+                    encodeURIComponent(
+                        placeholders.iframe.replace(reURLPlaceholder, src)
+                    );
                 replaced = false;
                 // Using contentWindow.location prevent tainting browser
                 // history -- i.e. breaking back button (seen on Chromium).
@@ -189,148 +204,125 @@ var collapser = (function() {
                 }
                 continue;
             }
-
-            // Everything else
-            target.setAttribute(srcProps[tagName], placeholders[tagName]);
+            target.setAttribute(src1stProps[tag], placeholders[tag]);
             target.style.setProperty('border', placeholders.border, 'important');
             target.style.setProperty('background', placeholders.background, 'important');
-        }
-
-        // Renew map: I believe that even if all properties are deleted, an
-        // object will still use more memory than a brand new one.
-        if ( pendingRequestCount === 0 ) {
-            pendingRequests = {};
         }
     };
 
     var send = function() {
-        timer = null;
-        vAPI.messaging.send('contentscript.js', {
-            what: 'evaluateURLs',
-            requests: newRequests
-        }, onProcessed);
-        newRequests = [];
+        processTimer = undefined;
+        toCollapse.set(resquestIdGenerator, toProcess);
+        var msg = {
+            what: 'lookupBlockedCollapsibles',
+            id: resquestIdGenerator,
+            toFilter: toFilter,
+            hash: cachedBlockedMapHash
+        };
+        vAPI.messaging.send('contentscript.js', msg, onProcessed);
+        toProcess = [];
+        toFilter = [];
+        resquestIdGenerator += 1;
     };
 
     var process = function(delay) {
-        if ( newRequests.length === 0 ) {
-            return;
-        }
+        if ( toProcess.length === 0 ) { return; }
         if ( delay === 0 ) {
-            clearTimeout(timer);
+            if ( processTimer !== undefined ) {
+                clearTimeout(processTimer);
+            }
             send();
-        } else if ( timer === null ) {
-            timer = vAPI.setTimeout(send, delay || 50);
+        } else if ( processTimer === undefined ) {
+            processTimer = vAPI.setTimeout(send, delay || 47);
+        }
+    };
+
+    var add = function(target) {
+        toProcess.push(target);
+    };
+
+    var addMany = function(targets) {
+        var i = targets.length;
+        while ( i-- ) {
+            toProcess.push(targets[i]);
         }
     };
 
     var iframeSourceModified = function(mutations) {
         var i = mutations.length;
         while ( i-- ) {
-            addFrameNode(mutations[i].target, true);
+            addIFrame(mutations[i].target, true);
         }
         process();
     };
-    var iframeSourceObserver = null;
+    var iframeSourceObserver;
     var iframeSourceObserverOptions = {
         attributes: true,
         attributeFilter: [ 'src' ]
     };
 
-    var addFrameNode = function(iframe, dontObserve) {
+    var addIFrame = function(iframe, dontObserve) {
         // https://github.com/gorhill/uBlock/issues/162
         // Be prepared to deal with possible change of src attribute.
         if ( dontObserve !== true ) {
-            if ( iframeSourceObserver === null ) {
+            if ( iframeSourceObserver === undefined ) {
                 iframeSourceObserver = new MutationObserver(iframeSourceModified);
             }
             iframeSourceObserver.observe(iframe, iframeSourceObserverOptions);
         }
-        // https://github.com/chrisaljoudi/uBlock/issues/174
-        // Do not remove fragment from src URL
         var src = iframe.src;
-        if ( src.lastIndexOf('http', 0) !== 0 ) {
-            return;
-        }
-        var req = new PendingRequest(iframe);
-        newRequests.push(new BouncingRequest(req.id, 'iframe', src));
+        if ( src === '' || typeof src !== 'string' ) { return; }
+        if ( src.startsWith('http') === false ) { return; }
+        toFilter.push({ type: 'frame', url: iframe.src });
+        add(iframe);
     };
 
-    var addNode = function(target) {
-        var tagName = target.localName;
-        if ( tagName === 'iframe' ) {
-            addFrameNode(target);
-            return;
-        }
-        var prop = srcProps[tagName];
-        if ( prop === undefined ) {
-            return;
-        }
-        // https://github.com/chrisaljoudi/uBlock/issues/174
-        // Do not remove fragment from src URL
-        var src = target[prop];
-        if ( typeof src !== 'string' || src === '' ) {
-            return;
-        }
-        if ( src.lastIndexOf('http', 0) !== 0 ) {
-            return;
-        }
-        var req = new PendingRequest(target);
-        newRequests.push(new BouncingRequest(req.id, tagName, src));
-    };
-
-    var addNodes = function(nodes) {
-        var node;
-        var i = nodes.length;
+    var addIFrames = function(iframes) {
+        var i = iframes.length;
         while ( i-- ) {
-            node = nodes[i];
-            if ( node.nodeType === 1 ) {
-                addNode(node);
+            addIFrame(iframes[i]);
+        }
+    };
+
+    var addNodeList = function(nodeList) {
+        var node,
+            i = nodeList.length;
+        while ( i-- ) {
+            node = nodeList[i];
+            if ( node.nodeType !== 1 ) { continue; }
+            if ( node.localName === 'iframe' ) {
+                addIFrame(node);
+            }
+            if ( node.childElementCount !== 0 ) {
+                addIFrames(node.querySelectorAll('iframe'));
             }
         }
     };
 
-    var addBranches = function(branches) {
-        var root;
-        var i = branches.length;
-        while ( i-- ) {
-            root = branches[i];
-            if ( root.nodeType === 1 ) {
-                addNode(root);
-                // blocked images will be reported by onResourceFailed
-                addNodes(root.querySelectorAll('iframe'));
-            }
-        }
-    };
-
-    // Listener to collapse blocked resources.
-    // - Future requests not blocked yet
-    // - Elements dynamically added to the page
-    // - Elements which resource URL changes
     var onResourceFailed = function(ev) {
-        addNode(ev.target);
-        process();
+        if ( tagToTypeMap[ev.target.localName] !== undefined ) {
+            add(ev.target);
+            process();
+        }
     };
     document.addEventListener('error', onResourceFailed, true);
 
     vAPI.shutdown.add(function() {
-        if ( timer !== null ) {
-            clearTimeout(timer);
-            timer = null;
-        }
-        if ( iframeSourceObserver !== null ) {
-            iframeSourceObserver.disconnect();
-            iframeSourceObserver = null;
-        }
         document.removeEventListener('error', onResourceFailed, true);
-        newRequests = [];
-        pendingRequests = {};
-        pendingRequestCount = 0;
+        if ( iframeSourceObserver !== undefined ) {
+            iframeSourceObserver.disconnect();
+            iframeSourceObserver = undefined;
+        }
+        if ( processTimer !== undefined ) {
+            clearTimeout(processTimer);
+            processTimer = undefined;
+        }
     });
 
     return {
-        addNodes: addNodes,
-        addBranches: addBranches,
+        addMany: addMany,
+        addIFrames: addIFrames,
+        addNodeList: addNodeList,
         process: process
     };
 })();
@@ -345,10 +337,6 @@ var hasInlineScript = function(nodeList, summary) {
         if ( node.nodeType !== 1 ) {
             continue;
         }
-        if ( typeof node.localName !== 'string' ) {
-            continue;
-        }
-
         if ( node.localName === 'script' ) {
             text = node.textContent.trim();
             if ( text === '' ) {
@@ -357,7 +345,6 @@ var hasInlineScript = function(nodeList, summary) {
             summary.inlineScript = true;
             break;
         }
-
         if ( node.localName === 'a' && node.href.lastIndexOf('javascript', 0) === 0 ) {
             summary.inlineScript = true;
             break;
@@ -367,8 +354,6 @@ var hasInlineScript = function(nodeList, summary) {
         summary.mustReport = true;
     }
 };
-
-/******************************************************************************/
 
 var nodeListsAddedHandler = function(nodeLists) {
     var i = nodeLists.length;
@@ -385,7 +370,7 @@ var nodeListsAddedHandler = function(nodeLists) {
         if ( summary.inlineScript === false ) {
             hasInlineScript(nodeLists[i], summary);
         }
-        collapser.addBranches(nodeLists[i]);
+        collapser.addNodeList(nodeLists[i]);
     }
     if ( summary.mustReport ) {
         vAPI.messaging.send('contentscript.js', summary);
@@ -415,7 +400,8 @@ var nodeListsAddedHandler = function(nodeLists) {
 
     vAPI.messaging.send('contentscript.js', summary);
 
-    collapser.addNodes(document.querySelectorAll('iframe,img'));
+    collapser.addMany(document.querySelectorAll('img'));
+    collapser.addIFrames(document.querySelectorAll('iframe'));
     collapser.process();
 })();
 
@@ -427,6 +413,9 @@ var nodeListsAddedHandler = function(nodeLists) {
 // Added node lists will be cumulated here before being processed
 
 (function() {
+    // This fixes http://acid3.acidtests.org/
+    if ( !document.body ) { return; }
+
     var addedNodeLists = [];
     var addedNodeListsTimer = null;
 
@@ -439,27 +428,18 @@ var nodeListsAddedHandler = function(nodeLists) {
     // https://github.com/gorhill/uBlock/issues/205
     // Do not handle added node directly from within mutation observer.
     var treeMutationObservedHandlerAsync = function(mutations) {
-        var iMutation = mutations.length;
-        var nodeList;
+        var iMutation = mutations.length,
+            nodeList;
         while ( iMutation-- ) {
             nodeList = mutations[iMutation].addedNodes;
             if ( nodeList.length !== 0 ) {
                 addedNodeLists.push(nodeList);
             }
         }
-        // I arbitrarily chose 250 ms for now:
-        // I have to compromise between the overhead of processing too few 
-        // nodes too often and the delay of many nodes less often. There is nothing
-        // time critical here.
         if ( addedNodeListsTimer === null ) {
-            addedNodeListsTimer = vAPI.setTimeout(treeMutationObservedHandler, 250);
+            addedNodeListsTimer = vAPI.setTimeout(treeMutationObservedHandler, 47);
         }
     };
-
-    // This fixes http://acid3.acidtests.org/
-    if ( !document.body ) {
-        return;
-    }
 
     // https://github.com/gorhill/httpswitchboard/issues/176
     var treeObserver = new MutationObserver(treeMutationObservedHandlerAsync);

src/js/matrix.js
@@ -39,6 +39,9 @@ var uniqueIdGenerator = 1;
 var Matrix = function() {
     this.id = uniqueIdGenerator++;
     this.reset();
+    this.sourceRegister = '';
+    this.decomposedSourceRegister = [''];
+    this.specificityRegister = 0;
 };
 
 /******************************************************************************/
@@ -141,9 +144,7 @@ var isIPAddress = function(hostname) {
 /******************************************************************************/
 
 var toBroaderHostname = function(hostname) {
-    if ( hostname === '*' ) {
-        return '';
-    }
+    if ( hostname === '*' ) { return ''; }
     if ( isIPAddress(hostname) ) {
         return toBroaderIPAddress(hostname);
     }
@@ -188,6 +189,20 @@ Matrix.prototype.reset = function() {
     this.rules = new Map();
     this.rootValue = Matrix.RedIndirect;
     this.modifiedTime = 0;
+};
+
+/******************************************************************************/
+
+Matrix.prototype.decomposeSource = function(srcHostname) {
+    if ( srcHostname === this.sourceRegister ) { return; }
+    var hn = srcHostname;
+    this.decomposedSourceRegister[0] = this.sourceRegister = hn;
+    var i = 1;
+    for (;;) {
+        hn = toBroaderHostname(hn);
+        this.decomposedSourceRegister[i++] = hn;
+        if ( hn === '' ) { break; }
+    }
 };
 
 /******************************************************************************/
@@ -331,10 +346,13 @@ Matrix.prototype.evaluateCell = function(srcHostname, desHostname, type) {
 /******************************************************************************/
 
 Matrix.prototype.evaluateCellZ = function(srcHostname, desHostname, type) {
-    var bitOffset = typeBitOffsets.get(type);
-    var s = srcHostname;
-    var v;
+    this.decomposeSource(srcHostname);
+
+    var bitOffset = typeBitOffsets.get(type),
+        s, v, i = 0;
     for (;;) {
+        s = this.decomposedSourceRegister[i++];
+        if ( s === '' ) { break; }
         v = this.rules.get(s + ' ' + desHostname);
         if ( v !== undefined ) {
             v = v >> bitOffset & 3;
@@ -342,9 +360,6 @@ Matrix.prototype.evaluateCellZ = function(srcHostname, desHostname, type) {
                 return v;
             }
         }
-        // TODO: external rules? (for presets)
-        s = toBroaderHostname(s);
-        if ( s === '' ) { break; }
     }
     // srcHostname is '*' at this point
 
@@ -366,6 +381,7 @@ Matrix.prototype.evaluateCellZ = function(srcHostname, desHostname, type) {
 
 Matrix.prototype.evaluateCellZXY = function(srcHostname, desHostname, type) {
     // Matrix filtering switch
+    this.specificityRegister = 0;
     if ( this.evaluateSwitchZ('matrix-off', srcHostname) ) {
         return Matrix.GreenIndirect;
     }
@@ -377,11 +393,13 @@ Matrix.prototype.evaluateCellZXY = function(srcHostname, desHostname, type) {
     // evaluating net requests.
 
     // Specific-hostname specific-type cell
+    this.specificityRegister = 1;
     var r = this.evaluateCellZ(srcHostname, desHostname, type);
     if ( r === 1 ) { return Matrix.RedDirect; }
     if ( r === 2 ) { return Matrix.GreenDirect; }
 
     // Specific-hostname any-type cell
+    this.specificityRegister = 2;
     var rl = this.evaluateCellZ(srcHostname, desHostname, '*');
     if ( rl === 1 ) { return Matrix.RedIndirect; }
 
@@ -390,10 +408,9 @@ Matrix.prototype.evaluateCellZXY = function(srcHostname, desHostname, type) {
 
     // Ancestor cells, up to 1st-party destination domain
     if ( firstPartyDesDomain !== '' ) {
+        this.specificityRegister = 3;
         for (;;) {
-            if ( d === firstPartyDesDomain ) {
-                break;
-            }
+            if ( d === firstPartyDesDomain ) { break; }
             d = d.slice(d.indexOf('.') + 1);
 
             // specific-hostname specific-type cell
@@ -420,11 +437,10 @@ Matrix.prototype.evaluateCellZXY = function(srcHostname, desHostname, type) {
     }
 
     // Keep going, up to root
+    this.specificityRegister = 4;
     for (;;) {
         d = toBroaderHostname(d);
-        if ( d === '*' ) {
-            break;
-        }
+        if ( d === '*' ) { break; }
 
         // specific-hostname specific-type cell
         r = this.evaluateCellZ(srcHostname, d, type);
@@ -438,6 +454,7 @@ Matrix.prototype.evaluateCellZXY = function(srcHostname, desHostname, type) {
     }
 
     // Any-hostname specific-type cells
+    this.specificityRegister = 5;
     r = this.evaluateCellZ(srcHostname, '*', type);
     // Line below is strict-blocking
     if ( r === 1 ) { return Matrix.RedIndirect; }
@@ -446,6 +463,7 @@ Matrix.prototype.evaluateCellZXY = function(srcHostname, desHostname, type) {
     if ( r === 2 ) { return Matrix.GreenIndirect; }
 
     // Any-hostname any-type cell
+    this.specificityRegister = 6;
     r = this.evaluateCellZ(srcHostname, '*', '*');
     if ( r === 1 ) { return Matrix.RedIndirect; }
     if ( r === 2 ) { return Matrix.GreenIndirect; }
@@ -534,22 +552,20 @@ Matrix.prototype.evaluateSwitch = function(switchName, srcHostname) {
 
 Matrix.prototype.evaluateSwitchZ = function(switchName, srcHostname) {
     var bitOffset = switchBitOffsets.get(switchName);
-    if ( bitOffset === undefined ) {
-        return false;
-    }
-    var bits;
-    var s = srcHostname;
+    if ( bitOffset === undefined ) { return false; }
+
+    this.decomposeSource(srcHostname);
+
+    var s, bits, i = 0;
     for (;;) {
+        s = this.decomposedSourceRegister[i++];
+        if ( s === '' ) { break; }
         bits = this.switches.get(s) || 0;
         if ( bits !== 0 ) {
             bits = bits >> bitOffset & 3;
             if ( bits !== 0 ) {
                 return bits === 1;
             }
-        }
-        s = toBroaderHostname(s);
-        if ( s === '' ) {
-            break;
         }
     }
     return false;

commit 6e37531a17591cb1be608bc41993ba1870a97058

fix noscript setting not being saved on change (regression from 68dedc8111)

src/js/settings.js
@@ -106,6 +106,7 @@ function onInputChanged(ev) {
         changeUserSettings(target.id, target.checked);
         break;
     case 'noMixedContent':
+    case 'noscriptTagsSpoofed':
     case 'processReferer':
         changeMatrixSwitch(
             target.getAttribute('data-matrix-switch'),